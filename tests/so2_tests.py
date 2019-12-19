2485ab3f9df051f3bb7d978bb024286f112c938d
Brendon Forsgren <bforsgren29@gmail.com>
2019-12-18 22:28:17 -0700

    Added a small framework for the so2_tests.py class to test each of the current functions

---
+++ tests/so2_tests.py
@@ -0,0 +1,24 @@
+import unittest
+import sys 
+sys.path.append(..)
+import so2
+
+class SO2Test(unittest.TestCase):
+
+    def testExp(self):
+        debug = 1
+
+    def testLog(self):
+        debug = 1
+
+    def testHat(self):
+        debug = 1
+
+    def testVee(self):
+        debug = 1
+
+    def testBoxPlus(self):
+        debug = 1
+
+    def testBoxMinus(self):
+        debug = 1
