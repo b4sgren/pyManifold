222ac348d1cc8ab1d4ee516466b3c2af38fdbcff
Brendon Forsgren <bforsgren29@gmail.com>
2019-12-23 16:53:19 -0700

    Inv passes 100 cases

--- tests/se2_tests.py
+++ tests/se2_tests.py
@@ -6,19 +6,21 @@ from se2 import SE2
 
 class SE2_Test(unittest.TestCase):
     def testInv(self):
-        t = np.array([1.0, 0.0])
-        theta = np.pi/2.0
-        ct = np.cos(theta)
-        st = np.sin(theta)
-        R = np.array([[ct, -st], [st, ct]])
-        T = SE2(R, t)
-        T_inv = T.inv()
-        T_inv_true = np.eye(3)
-        T_inv_true[:2, :2] = R.T 
-        T_inv_true[:2, 2] = -R.T @ t 
+        for i in range(100):
+            t = np.random.uniform(-10, 10, size=2)
+            theta = np.random.uniform(-np.pi, np.pi)
+            ct = np.cos(theta)
+            st = np.sin(theta)
+            R = np.array([[ct, -st], [st, ct]])
 
-        # self.assertAlmostEqual()
-        np.testing.assert_allclose(T_inv_true, T_inv.arr)
+            T = SE2(R, t)
+            T_inv = T.inv()
+            T_inv_true = np.eye(3)
+            T_inv_true[:2, :2] = R.T 
+            T_inv_true[:2, 2] = -R.T @ t 
+
+            # self.assertAlmostEqual()
+            np.testing.assert_allclose(T_inv_true, T_inv.arr)
 
 if __name__=="__main__":
     unittest.main()
