414fd015ce7158863e5f188f3940a33a09d7a46c
Brendon Forsgren <bforsgren29@gmail.com>
2019-12-28 08:51:07 -0700

    Tested passing a variable length array into the constructor for different initializations. Currently support rotation matrix and RPY angles

--- so3.py
+++ so3.py
@@ -11,5 +11,24 @@ G = np.array([[[0, 0, 0],
                 [0, 0, 0]]])
 
 class SO3:
-    def __init__(self, phi, theta, psi): # Add capability to check if phi is an 3x3 matrix instead of an angle
-        debug = 1
+    def __init__(self, args1, *args): # Add capability to check if phi is an 3x3 matrix instead of an angle
+        if len(args) == 0: #args1 is already a 3x3 rotation matrix
+            self.arr = args1 
+        elif len(args) == 2: #Arguments passed in are RPY angles
+            phi = args1
+            theta = args[0]
+            psi = args[1]
+
+            cps = np.cos(psi)
+            sps = np.sin(psi)
+            R1 = np.array([[cps, -sps, 0], [sps, cps, 0], [0, 0, 1]])
+
+            ct = np.cos(theta)
+            st = np.sin(theta)
+            R2 = np.array([[ct, 0, st], [0, 1, 0], [-st, 0, ct]])
+
+            cp = np.cos(phi)
+            sp = np.sin(phi)
+            R3 = np.array([[1, 0, 0], [0, cp, -sp], [0, sp, cp]])
+
+            self.arr = R1 @ R2 @ R3
--- tests/so3_tests.py
+++ tests/so3_tests.py
@@ -1,9 +1,33 @@
 import numpy as np 
 import unittest
+import sys
+sys.path.append("..")
+from so3 import SO3
 
 class SO3_testing(unittest.TestCase):
-    def ex(self):
-        debug = 1
+    def testConstructor(self):
+        for i in range(100):
+            angles = np.random.uniform(-np.pi, np.pi, size=3)
+            R_ex = SO3(angles[0], angles[1], angles[2])
+
+            cp = np.cos(angles[0])
+            sp = np.sin(angles[0])
+            R1 = np.array([[1, 0, 0], [0, cp, -sp], [0, sp, cp]])
+
+            ct = np.cos(angles[1])
+            st = np.sin(angles[1])
+            R2 = np.array([[ct, 0, st], [0, 1, 0], [-st, 0, ct]])
+
+            cps = np.cos(angles[2])
+            sps = np.sin(angles[2])
+            R3 = np.array([[cps, -sps, 0], [sps, cps, 0], [0, 0, 1]])
+
+            R_true = R3 @ R2 @ R1 
+
+            R_ex2 = SO3(R_true)
+
+            np.testing.assert_allclose(R_true, R_ex.arr)
+            np.testing.assert_allclose(R_true, R_ex2.arr)
     
 
 if __name__=="__main__":
