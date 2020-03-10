c36e65c13ca989d712d897dcc9744eca487590f6
Brendon Forsgren <bforsgren29@gmail.com>
2020-03-09 21:46:28 -0600

    Added a subtraction operator. It is mostly for use by the class. I can't see why it would be used else where since direct subtraction isn't defined

--- so3.py
+++ so3.py
@@ -72,10 +72,10 @@ class SO3:
     def log(R):
         theta = np.arccos((np.trace(R.arr) - 1)/2.0)
         if theta > 1e-3:
-            return theta / (2.0 * np.sin(theta)) * (R.R - R.R.T) # Define subtraction operator
+            return theta / (2.0 * np.sin(theta)) * (R - R.transpose()) # Define subtraction operator
         else: # Do taylor series expansion
             temp = 1/2.0 * (1 + theta**2 / 3.0 + 7 * theta**4 / 360)
-            return temp * (R.R - R.R.T)
+            return temp * (R - R.transpose())
     
     @classmethod 
     def exp(cls, logR):
