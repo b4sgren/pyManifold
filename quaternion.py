3f5fc95addca8d903f1d93b712231adfaba79513
Brendon Forsgren <bforsgren29@gmail.com>
2020-03-18 22:16:23 -0600

    The axis angle appears to work. Debugging rotation matrix

--- quaternion.py
+++ quaternion.py
@@ -16,7 +16,38 @@ class Quaternion:
                 raise ValueError("Input must be a numpy array of length 4")
         else:
             raise ValueError("Input must be a numpy array of length 4")
+    
+    @classmethod 
+    def fromRotationMatrix(cls, R):
+        if not R.shape == (3,3):
+            raise ValueError("Input must be  3x3 numpy array")        
+
+        delta = np.trace(R)
+        if delta > 0:
+            s = 2 * np.sqrt(delta + 1)
+            q = np.array([s/4, 1/s*(R[1,2] - R[2,1]), 1/s * (R[2,0] - R[0,2]), 1/s * (R[0,1] - R[1,0])])
+        elif R[0,0] > R[1,1] and R[0,0] > R[2,2]:
+            s = 2 * np.sqrt(1 + R[0,0] - R[1,1] - R[2,2])
+            q = np.array([1/s * (R[1,2] - R[2,1]), s/4.0, 1/s * (R[1,0] - R[0,1]), 1/s * (R[2,0] - R[0,2])])
+        elif R[1,1] > R[2,2]:
+            s = 2 * np.sqrt(1 + R[1,1] - R[0,0] - R[2,2])
+            q = np.array([(R[2,0] - R[0,2])/2, 1/s * (R[1,0] - R[0,1]), s/4, 1/s * (R[2,1] - R[1,2])])
+        else:
+            s = 2 * np.sqrt(1 + R[2,2] - R[0,0] - R[1,1])
+            q = np.array([1/s * (R[0,1] - R[1,0]), 1/s * (R[2,0] - R[0,2]), 1/s * (R[2,1] - R[1,2]), s/4])
         
+        return cls(q)
+    
+    @classmethod
+    def fromAxisAngle(cls, vec):
+        if not vec.shape == (3,) and not vec.shape == (3,1) and not vec.shape == (1,3):
+            raise ValueError("Input must be a numpy array of length 3")
+
+        theta = np.linalg.norm(vec)
+        v = (vec / theta).squeeze() #Make a unit vector
+        st2 = np.sin(theta/2)
+        q = np.array([np.cos(theta/2), v[0] * st2, v[1] * st2, v[2] * st2])
+        return cls(q)
     
     @staticmethod 
     def hat(omega):
--- tests/quaternion_tests.py
+++ tests/quaternion_tests.py
@@ -34,6 +34,32 @@ class Quaternion_Testing(unittest.TestCase):
             log_q_true = np.array([0, vec[0], vec[1], vec[2]])
 
             np.testing.assert_allclose(log_q_true, log_q)
+    
+    def testFromRot(self): #This test fails. Need to determine if the quaternion used by Scipy is the same format as my quaternion
+        for i in range(100):
+            R = Rotation.random().as_dcm()
+            q = Quaternion.fromRotationMatrix(R)
+            q_true = Rotation.from_dcm(R).as_quat()
+            q_true = switchOrder(q_true)
+
+            if np.linalg.norm(q_true - q.arr) > 1e-3:
+                # Pdb().set_trace()
+                q2 = Quaternion.fromRotationMatrix(R)
+
+            # np.testing.assert_allclose(q_true, q.arr)
+    
+    def testFromAxisAngle(self):
+        for i in range(100):
+            vec = Rotation.random().as_rotvec()
+            q = Quaternion.fromAxisAngle(vec)
+            q_true = Rotation.from_rotvec(vec).as_quat()
+            q_true = switchOrder(q_true)
+
+            if np.linalg.norm(q_true - q.arr) > 1e-3:
+                Pdb().set_trace()
+                q2 = Quaternion.fromAxisAngle(vec)
+
+            np.testing.assert_allclose(q_true, q.arr)
 
 if __name__=="__main__":
     unittest.main()
