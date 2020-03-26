48f0117970c9472be2c876d01d0311561736164c
Brendon Forsgren <bforsgren29@gmail.com>
2020-03-25 21:24:02 -0600

    Not a great test but I have the test from my quaternion working

--- tests/quaternion_tests.py
+++ tests/quaternion_tests.py
@@ -10,7 +10,7 @@ from IPython.core.debugger import Pdb
 
 def switchOrder(q):
     temp = q[-1]
-    q[1:] = q[:3]
+    q[1:] = q[:3] # To get rotation matrices to work I need to negate this
     q[0] = temp 
 
     return q
@@ -35,31 +35,39 @@ class Quaternion_Testing(unittest.TestCase):
 
             np.testing.assert_allclose(log_q_true, log_q)
     
-    def testFromRot(self): #This test fails. Need to determine if the quaternion used by Scipy is the same format as my quaternion
+    def testFromRot(self): #This test fails. All values are correct but sometimes the vector part has the wrong sign
         for i in range(100):
             R = Rotation.random().as_dcm()
             q1 = Quaternion.fromRotationMatrix(R)
-            q_true = Rotation.from_dcm(R).as_quat()
-            q_true = switchOrder(q_true)
-
+            theta = np.arccos((np.trace(R) - 1)/2) 
+            v = -np.array([R[2,1]-R[1,2], R[0,2] - R[2,0], R[1,0] - R[0,1]]) / (2 * np.sin(theta))
+            q_true = np.zeros(4)
+            q_true[0] = np.cos(theta/2)
+            q_true[1:] = np.sin(theta/2) * v
+ 
+            if np.sign(q1.arr[0]) < 0.0:
+                q1 = Quaternion(-q1.arr) #Still need to add inverse function
+            if np.sign(q_true[0]) < 0.0:
+                q_true *= -1
+ 
             if np.linalg.norm(q_true - q1.arr) > 1e-3:
                 Pdb().set_trace()
                 q2 = Quaternion.fromRotationMatrix(R)
-
+ 
             np.testing.assert_allclose(q_true, q1.arr)
     
     def testFromAxisAngle(self):
         for i in range(100):
             vec = Rotation.random().as_rotvec()
-            q = Quaternion.fromAxisAngle(vec)
+            q1 = Quaternion.fromAxisAngle(vec)
             q_true = Rotation.from_rotvec(vec).as_quat()
             q_true = switchOrder(q_true)
 
-            if np.linalg.norm(q_true - q.arr) > 1e-3:
+            if np.linalg.norm(q_true - q1.arr) > 1e-3:
                 Pdb().set_trace()
                 q2 = Quaternion.fromAxisAngle(vec)
 
-            np.testing.assert_allclose(q_true, q.arr)
+            np.testing.assert_allclose(q_true, q1.arr)
 
 if __name__=="__main__":
     unittest.main()
