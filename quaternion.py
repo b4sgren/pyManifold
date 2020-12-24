I have the quaternion jacobians for composition written. I'm getting a wierd artifact that makes me question if my implementation is correct

quaternion.py
@@ -56,7 +56,7 @@ class Quaternion:
     def __repr__(self):
         return f'[{self.qw} + {self.qx}i + {self.qy}j + {self.qz}k]'
 
-    def otimes(self, q):
+    def otimes(self, q): # Does this do the wrong thing? R1*R2 = q2 * q1 if I'm not mistaken for quaternions
         Q = np.block([[self.qw, -self.qv], [self.qv[:,None], self.qw * np.eye(3) + self.skew()]]) #Typo in Jame's stuff. See QUat for Err State KF
         return Quaternion(Q @ q.q)
 
@@ -107,6 +107,13 @@ class Quaternion:
     def boxminusl(self, q):
         assert isinstance(q, Quaternion)
         return Quaternion.Log(self * q.inv())
+
+    def compose(self, q, Jr=False, Jl=False):
+        res = self * q
+        if Jr:
+            return res, q.inv().Adj
+        if Jl:
+            return res, np.eye(3)
 
     @classmethod
     def random(cls): #Method found at planning.cs.uiuc.edu/node198.html (SO how to generate a random quaternion quickly)

tests/test_quaternion.py
@@ -51,11 +51,6 @@ class Quaternion_Testing(unittest.TestCase):
 
     def testRotationMatrixFromQuaternion(self):
         for i in range(100):
-            # R_true = SO3.random().R
-            # q = Quaternion.fromRotationMatrix(R_true)
-            # R = q.R
-
-            # np.testing.assert_allclose(R_true, R)
             q = Quaternion.random()
             R = q.R
             q2 = Quaternion.fromRotationMatrix(R)
@@ -251,6 +246,29 @@ class Quaternion_Testing(unittest.TestCase):
         Jl_true = Adj_qinv @ Jr @ np.linalg.inv(Adj_q)
 
         np.testing.assert_allclose(Jl_true, Jl)
+
+    def test_right_jacobian_of_composition(self):
+        q1 = Quaternion.random()
+        q2 = Quaternion.random()
+
+        q3, Jr = q1.compose(q2, Jr=True)
+        Jr_true = np.linalg.inv(q2.Adj)
+
+        np.testing.assert_allclose(Jr_true, Jr)
+
+    def test_left_jacobian_of_composition(self):
+        for i in range(100):
+            q1 = Quaternion.random()
+            q2 = Quaternion.random()
+
+            q3, Jr = q1.compose(q2, Jr=True)
+            _, Jl = q1. compose(q2, Jl=True)
+
+            # Jl_true = q3.Adj @ Jr @ q1.inv().Adj
+            # This is what works but is this an artifact of something being wrong with my quaternion class
+            Jl_true = q1.inv().Adj @ Jr @ q3.Adj
+
+            np.testing.assert_allclose(Jl_true, Jl, atol=1e-10)
 
 
 if __name__=="__main__":