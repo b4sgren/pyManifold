Fixed some issues. Added the estimated robot. Next step is propagate covariance

examples/error_state_ekf_se2.py
@@ -11,25 +11,42 @@ if __name__=="__main__":
   dt = 0.1
 
   true_robot = Robot()
+  est_robot = Robot()
 
-  state_hist = [true_robot.state]
+  state_hist = []
+  est_hist = []
+  state_hist.append([true_robot.state.x, true_robot.state.y,\
+                    true_robot.state.theta])
+  est_hist.append([est_robot.state.x, est_robot.state.y,\
+                    est_robot.state.theta])
+
   t_hist = [t]
 
   while t < tf:
-    u = true_robot.getInputs(t)
+    u, u_hat = true_robot.getInputs(t)
     true_robot.propogateDynamics(u, dt)
+    est_robot.propogateDynamics(u_hat, dt)
 
     t += dt
-    state_hist.append(true_robot.state)
+    state_hist.append([true_robot.state.x, true_robot.state.y, \
+                      true_robot.state.theta])
+    est_hist.append([est_robot.state.x, est_robot.state.y, \
+                      est_robot.state.theta])
+
     t_hist.append(t)
 
   state_hist = np.array([state_hist]).T
+  est_hist = np.array([est_hist]).T
   fig, ax = plt.subplots(3,1)
   ax[0].plot(t_hist, state_hist[0])
+  ax[0].plot(t_hist, est_hist[0])
   ax[1].plot(t_hist, state_hist[1])
+  ax[1].plot(t_hist, est_hist[1])
   ax[2].plot(t_hist, state_hist[2])
+  ax[2].plot(t_hist, est_hist[2])
 
   plt.figure(2)
   plt.plot(state_hist[0], state_hist[1])
+  plt.plot(est_hist[0], est_hist[1])
 
   plt.show()

examples/se2_classes.py
@@ -6,15 +6,20 @@ from se2 import SE2
 class Robot:
   def __init__(self):
     self._state = SE2.Identity()
+    self._odom_cov = np.diag([.1, 1e-3])
 
   @property
   def state(self):
-    return SE2.Log(self._state)
+    return self._state
 
   def getInputs(self, t):
-    v = 1 #+ .5 * np.cos(2*np.pi*0.2*t)
-    w = -0.2 #+ 2*np.cos(2*np.pi*0.6*t)
-    return np.array([v,w])
+    v = 1 + .5 * np.cos(2*np.pi*0.2*t)
+    w = -0.2 + 2*np.cos(2*np.pi*0.6*t)
+
+    u = np.array([v, w])
+    u_hat = u + np.random.multivariate_normal(np.zeros(2), self._odom_cov)
+
+    return u, u_hat
 
   def propogateDynamics(self, u, dt):
     v,w = u[0], u[1]