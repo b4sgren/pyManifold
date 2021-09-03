Got my jacobians working for the Quaternion classgit add jacobian_tester.py  Now to fix the jacobians for SE3

examples/se2_classes.py
@@ -50,10 +50,8 @@ class SE2_EKF:
     self._cov = np.zeros((3,3))
 
   def propogateDynamics(self, robot, u, dt):
-    # temp, F = robot.state.boxplusr(u*dt, Jr=np.eye(3))
-    # _, G = SE2.Exp(u*dt, Jr=np.eye(3))
-    F = np.linalg.inv(SE2.Exp(u*dt).Adj)
-    _, G = SE2.Exp(u*dt, Jr=np.eye(3))
+    U, G = SE2.Exp(u*dt, Jr=np.eye(3))
+    _, F = robot.state.compose(U, Jr=np.eye(3))
 
     self._cov = F @ self._cov @ F.T + G @ robot.odom_cov @ G.T
 
@@ -61,6 +59,7 @@ class SE2_EKF:
     for zi, lm in zip(z, lms):
       T_inv, J1 = robot.state.inv(Jr=np.eye(3))
       z_hat, J2 = T_inv.transa(lm, Jr=np.eye(3))
+      # Compose Jacobians according to chain rule
       H = J2 @ J1
 
       # Calculate Innovation

tests/test_quaternion.py
@@ -258,200 +258,201 @@ class Quaternion_Testing(unittest.TestCase):
             q2 = Quaternion.fromRPY(rpy)
             np.testing.assert_allclose(q1.q, q2.q)
 
-    # def test_right_jacobian_of_inversion(self):
-    #     q = Quaternion.random()
-    #     q_inv, Jr = q.inv(Jr=np.eye(3))
-    #     Jr_true = -q.R.T
+    def test_right_jacobian_of_inversion(self):
+        q = Quaternion.random()
+        q_inv, Jr = q.inv(Jr=np.eye(3))
+        Jr_true = -q.R.T
 
-    #     np.testing.assert_allclose(Jr_true, Jr)
+        np.testing.assert_allclose(Jr_true, Jr)
 
-    # def test_left_jacobian_of_inversion(self):
-    #     q = Quaternion.random()
-    #     q_inv, Jr = q.inv(Jr=np.eye(3))
-    #     _, Jl = q.inv(Jl=np.eye(3))
+    def test_left_jacobian_of_inversion(self):
+        q = Quaternion.random()
+        q_inv, Jr = q.inv(Jr=np.eye(3))
+        _, Jl = q.inv(Jl=np.eye(3))
 
-    #     Adj_q = q.Adj
-    #     Adj_qinv = q_inv.Adj
-    #     Jl_true = Adj_qinv @ Jr @ np.linalg.inv(Adj_q)
+        Adj_q = q.Adj
+        Adj_qinv = q_inv.Adj
+        Jl_true = Adj_qinv @ Jr @ np.linalg.inv(Adj_q)
 
-    #     np.testing.assert_allclose(Jl_true, Jl)
+        np.testing.assert_allclose(Jl_true, Jl)
 
-    # def test_right_jacobian_of_composition(self):
-    #     q1 = Quaternion.random()
-    #     q2 = Quaternion.random()
+    def test_right_jacobian_of_composition(self):
+        q1 = Quaternion.random()
+        q2 = Quaternion.random()
 
-    #     q3, Jr = q1.compose(q2, Jr=np.eye(3))
-    #     Jr_true = np.linalg.inv(q2.Adj)
+        q3, Jr = q1.compose(q2, Jr=np.eye(3))
+        Jr_true = np.linalg.inv(q2.Adj)
 
-    #     np.testing.assert_allclose(Jr_true, Jr)
+        np.testing.assert_allclose(Jr_true, Jr)
 
-    # def test_left_jacobian_of_composition(self):
-    #     for i in range(100):
-    #         q1 = Quaternion.random()
-    #         q2 = Quaternion.random()
+    def test_left_jacobian_of_composition(self):
+        for i in range(100):
+            q1 = Quaternion.random()
+            q2 = Quaternion.random()
 
-    #         q3, Jr = q1.compose(q2, Jr=np.eye(3))
-    #         _, Jl = q1. compose(q2, Jl=np.eye(3))
+            q3, Jr = q1.compose(q2, Jr=np.eye(3))
+            _, Jl = q1. compose(q2, Jl=np.eye(3))
 
-    #         Jl_true = q3.Adj @ Jr @ q1.inv().Adj
+            Jl_true = q3.Adj @ Jr @ q1.inv().Adj
 
-    #         np.testing.assert_allclose(Jl_true, Jl, atol=1e-10)
+            np.testing.assert_allclose(Jl_true, Jl, atol=1e-10)
 
-    # def test_jacobians_of_exponential(self):
-    #     for i in range(100):
-    #         tau = np.random.uniform(-np.pi, np.pi, size=3)
-    #         q, Jr = Quaternion.Exp(tau, Jr=np.eye(3))
-    #         _, Jl = Quaternion.Exp(-tau, Jl=np.eye(3))
+    def test_jacobians_of_exponential(self):
+        for i in range(100):
+            tau = np.random.uniform(-np.pi, np.pi, size=3)
+            q, Jr = Quaternion.Exp(tau, Jr=np.eye(3))
+            _, Jl = Quaternion.Exp(-tau, Jl=np.eye(3))
 
-    #         np.testing.assert_allclose(Jl, Jr)
+            np.testing.assert_allclose(Jl, Jr)
 
-    # def test_right_jacobian_of_logarithm(self):
-    #     for i in range(100):
-    #         q = Quaternion.random()
-    #         logq, Jr_inv = Quaternion.Log(q, Jr=np.eye(3))
-    #         _, Jr = Quaternion.Exp(logq, Jr=np.eye(3))
+    def test_right_jacobian_of_logarithm(self):
+        for i in range(100):
+            q = Quaternion.random()
+            logq, Jr_inv = Quaternion.Log(q, Jr=np.eye(3))
+            _, Jr = Quaternion.Exp(logq, Jr=np.eye(3))
 
-    #         np.testing.assert_allclose(np.linalg.inv(Jr), Jr_inv)
+            np.testing.assert_allclose(np.linalg.inv(Jr), Jr_inv)
 
-    # def test_left_jacobian_or_logarithm(self):
-    #     for i in range(100):
-    #         q = Quaternion.random()
-    #         logq, Jl_inv = Quaternion.Log(q, Jl=np.eye(3))
-    #         _, Jl = Quaternion.Exp(logq, Jl=np.eye(3))
+    def test_left_jacobian_or_logarithm(self):
+        for i in range(100):
+            q = Quaternion.random()
+            logq, Jl_inv = Quaternion.Log(q, Jl=np.eye(3))
+            _, Jl = Quaternion.Exp(logq, Jl=np.eye(3))
 
-    #         np.testing.assert_allclose(np.linalg.inv(Jl), Jl_inv)
+            np.testing.assert_allclose(np.linalg.inv(Jl), Jl_inv)
 
-    # def test_right_jacobian_of_rotation(self):
-    #     for i in range(100):
-    #         q = Quaternion.random()
-    #         v = np.random.uniform(-10, 10, size=3)
+    def test_right_jacobian_of_rotation(self):
+        for i in range(100):
+            q = Quaternion.random()
+            v = np.random.uniform(-10, 10, size=3)
 
-    #         vp, Jr = q.rota(v, Jr=np.eye(3))
-    #         Jr_true = -skew(q.R @ v) # left jacobian in James table... Need to study a little more
+            vp, Jr = q.rota(v, Jr=np.eye(3))
+            Jr_true = -q.R.T @ skew(v) # James equation missing a transpose.
+            # Confirmed by testing against numerical differentiation
 
-    #         np.testing.assert_allclose(Jr_true, Jr)
+            np.testing.assert_allclose(Jr_true, Jr)
 
-    # def test_left_jacobian_of_rotation(self):
-    #     for i in range(100):
-    #         q = Quaternion.random()
-    #         v = np.random.uniform(-10, 10, size=3)
+    def test_left_jacobian_of_rotation(self):
+        for i in range(100):
+            q = Quaternion.random()
+            v = np.random.uniform(-10, 10, size=3)
 
-    #         vp, Jl = q.rota(v, Jl=np.eye(3))
-    #         _, Jr = q.rota(v, Jr=np.eye(3))
+            vp, Jl = q.rota(v, Jl=np.eye(3))
+            _, Jr = q.rota(v, Jr=np.eye(3))
 
-    #         Jl_true = np.eye(3) @ Jr @ np.linalg.inv(q.Adj)
+            Jl_true = np.eye(3) @ Jr @ np.linalg.inv(q.Adj)
 
-    #         np.testing.assert_allclose(Jl_true, Jl, atol=1e-10)
+            np.testing.assert_allclose(Jl_true, Jl, atol=1e-10)
 
-    # def test_jacobians_of_composition_second_element(self):
-    #     for i in range(100):
-    #         q1 = Quaternion.random()
-    #         q2 = Quaternion.random()
+    def test_jacobians_of_composition_second_element(self):
+        for i in range(100):
+            q1 = Quaternion.random()
+            q2 = Quaternion.random()
 
-    #         q3, Jr2 = q1.compose(q2, Jr2=np.eye(3))
-    #         _, Jl2 = q1.compose(q2, Jl2=np.eye(3))
+            q3, Jr2 = q1.compose(q2, Jr2=np.eye(3))
+            _, Jl2 = q1.compose(q2, Jl2=np.eye(3))
 
-    #         Jl2_true = q3.Adj @ Jr2 @ np.linalg.inv(q2.Adj)
+            Jl2_true = q3.Adj @ Jr2 @ np.linalg.inv(q2.Adj)
 
-    #         np.testing.assert_allclose(Jl2_true, Jl2)
+            np.testing.assert_allclose(Jl2_true, Jl2)
 
-    # def test_right_jacobian_of_rotp(self):
-    #     for i in range(100):
-    #         q = Quaternion.random()
-    #         v = np.random.uniform(-10, 10, size=3)
+    def test_right_jacobian_of_rotp(self):
+        for i in range(100):
+            q = Quaternion.random()
+            v = np.random.uniform(-10, 10, size=3)
 
-    #         vp, Jr = q.rotp(v, Jr=np.eye(3))
-    #         vx = np.array([[0, -v[2], v[1]],
-    #                         [v[2], 0, -v[0]],
-    #                         [-v[1], v[0], 0]])
-    #         Jr_true = q.R.T @ skew(v) # left jacobian in james table...?
+            vp, Jr = q.rotp(v, Jr=np.eye(3))
+            vx = np.array([[0, -v[2], v[1]],
+                            [v[2], 0, -v[0]],
+                            [-v[1], v[0], 0]])
+            Jr_true = skew(q.R @ v) # left jacobian in james table...?
 
-    #         np.testing.assert_allclose(Jr_true, Jr, atol=1e-10)
+            np.testing.assert_allclose(Jr_true, Jr, atol=1e-10)
 
-    # # Rotating a vector isn't consistent across different ways
-    # def test_left_jacobian_of_rotp(self):
-    #     for i in range(100):
-    #         q = Quaternion.random()
-    #         v = np.random.uniform(-10, 10, size=3)
+    # Rotating a vector isn't consistent across different ways
+    def test_left_jacobian_of_rotp(self):
+        for i in range(100):
+            q = Quaternion.random()
+            v = np.random.uniform(-10, 10, size=3)
 
-    #         vp, Jl = q.rotp(v, Jl=np.eye(3))
-    #         _, Jr = q.rotp(v, Jr=np.eye(3))
+            vp, Jl = q.rotp(v, Jl=np.eye(3))
+            _, Jr = q.rotp(v, Jr=np.eye(3))
 
-    #         Jl_true = np.eye(3) @ Jr @ np.linalg.inv(q.Adj)
+            Jl_true = np.eye(3) @ Jr @ np.linalg.inv(q.Adj)
 
-    #         np.testing.assert_allclose(Jl_true, Jl, atol=1e-10)
+            np.testing.assert_allclose(Jl_true, Jl, atol=1e-10)
 
-    # def test_jacobians_of_boxplusr(self):
-    #     for i in range(100):
-    #         q = Quaternion.random()
-    #         theta = np.random.uniform(-np.pi, np.pi, size=3)
+    def test_jacobians_of_boxplusr(self):
+        for i in range(100):
+            q = Quaternion.random()
+            theta = np.random.uniform(-np.pi, np.pi, size=3)
 
-    #         q2, Jr = q.boxplusr(theta, Jr=np.eye(3))
-    #         _, Jl = q.boxplusr(theta, Jl=np.eye(3))
+            q2, Jr = q.boxplusr(theta, Jr=np.eye(3))
+            _, Jl = q.boxplusr(theta, Jl=np.eye(3))
 
-    #         Jl_true = q2.Adj @ Jr @ np.eye(3)
+            Jl_true = q2.Adj @ Jr @ np.eye(3)
 
-    #         np.testing.assert_allclose(Jl_true, Jl)
+            np.testing.assert_allclose(Jl_true, Jl)
 
-    # def test_right_jacobians_of_boxminusr(self):
-    #     for i in range(100):
-    #         q1, q2 = Quaternion.random(), Quaternion.random()
+    def test_right_jacobians_of_boxminusr(self):
+        for i in range(100):
+            q1, q2 = Quaternion.random(), Quaternion.random()
 
-    #         theta, Jr1 = q1.boxminusr(q2, Jr1=np.eye(3))
-    #         dq = q2.inv() * q1
-    #         _, Jr1_true = Quaternion.Log(dq, Jr=np.eye(3))
+            theta, Jr1 = q1.boxminusr(q2, Jr1=np.eye(3))
+            dq = q2.inv() * q1
+            _, Jr1_true = Quaternion.Log(dq, Jr=np.eye(3))
 
-    #         _, Jr2 = q1.boxminusr(q2, Jr2=np.eye(3))
-    #         _, Jr2_true = Quaternion.Log(dq, Jl=np.eye(3))
+            _, Jr2 = q1.boxminusr(q2, Jr2=np.eye(3))
+            _, Jr2_true = Quaternion.Log(dq, Jl=np.eye(3))
 
-    #         np.testing.assert_allclose(Jr1_true, Jr1)
-    #         np.testing.assert_allclose(-Jr2_true, Jr2)
+            np.testing.assert_allclose(Jr1_true, Jr1)
+            np.testing.assert_allclose(-Jr2_true, Jr2)
 
-    # def test_left_jacobians_of_boxminusr(self):
-    #     for i in range(100):
-    #         q1, q2 = Quaternion.random(), Quaternion.random()
+    def test_left_jacobians_of_boxminusr(self):
+        for i in range(100):
+            q1, q2 = Quaternion.random(), Quaternion.random()
 
-    #         theta, Jl1 = q1.boxminusr(q2, Jl1=np.eye(3))
-    #         _, Jr1 = q1.boxminusr(q2, Jr1=np.eye(3))
-    #         Jl1_true = np.eye(3) @ Jr1 @ q1.Adj.T
+            theta, Jl1 = q1.boxminusr(q2, Jl1=np.eye(3))
+            _, Jr1 = q1.boxminusr(q2, Jr1=np.eye(3))
+            Jl1_true = np.eye(3) @ Jr1 @ q1.Adj.T
 
-    #         _, Jl2 = q1.boxminusr(q2, Jl2=np.eye(3))
-    #         _, Jr2 = q1.boxminusr(q2, Jr2=np.eye(3))
-    #         Jl2_true = np.eye(3) @ Jr2 @ q2.Adj.T
+            _, Jl2 = q1.boxminusr(q2, Jl2=np.eye(3))
+            _, Jr2 = q1.boxminusr(q2, Jr2=np.eye(3))
+            Jl2_true = np.eye(3) @ Jr2 @ q2.Adj.T
 
-    #         np.testing.assert_allclose(Jl1_true, Jl1)
-    #         np.testing.assert_allclose(Jl2_true, Jl2)
+            np.testing.assert_allclose(Jl1_true, Jl1)
+            np.testing.assert_allclose(Jl2_true, Jl2)
 
-    # def test_jacobians_of_boxplusl(self):
-    #     for i in range(100):
-    #         q1 = Quaternion.random()
-    #         theta = np.random.uniform(-np.pi, np.pi, size=3)
+    def test_jacobians_of_boxplusl(self):
+        for i in range(100):
+            q1 = Quaternion.random()
+            theta = np.random.uniform(-np.pi, np.pi, size=3)
 
-    #         q2, Jr = q1.boxplusl(theta, Jr=np.eye(3))
-    #         _, Jl = q1.boxplusl(theta, Jl=np.eye(3))
+            q2, Jr = q1.boxplusl(theta, Jr=np.eye(3))
+            _, Jl = q1.boxplusl(theta, Jl=np.eye(3))
 
-    #         Jl_true = q2.Adj @ Jr @ np.eye(3)
-    #         np.testing.assert_allclose(Jl_true, Jl)
+            Jl_true = q2.Adj @ Jr @ np.eye(3)
+            np.testing.assert_allclose(Jl_true, Jl)
 
-    # def test_jacobians_of_boxminusl(self):
-    #     for i in range(100):
-    #         q1, q2 = Quaternion.random(), Quaternion.random()
+    def test_jacobians_of_boxminusl(self):
+        for i in range(100):
+            q1, q2 = Quaternion.random(), Quaternion.random()
 
-    #         theta, Jr = q1.boxminusl(q2, Jr1=np.eye(3))
-    #         _, Jl = q1.boxminusl(q2, Jl1=np.eye(3))
+            theta, Jr = q1.boxminusl(q2, Jr1=np.eye(3))
+            _, Jl = q1.boxminusl(q2, Jl1=np.eye(3))
 
-    #         Jl_true = np.eye(3) @ Jr @ q1.Adj.T
-    #         np.testing.assert_allclose(Jl_true, Jl)
+            Jl_true = np.eye(3) @ Jr @ q1.Adj.T
+            np.testing.assert_allclose(Jl_true, Jl)
 
-    # def test_jacobian_of_boxminusl_second_element(self):
-    #     for i in range(100):
-    #         q1, q2 = Quaternion.random(), Quaternion.random()
+    def test_jacobian_of_boxminusl_second_element(self):
+        for i in range(100):
+            q1, q2 = Quaternion.random(), Quaternion.random()
 
-    #         theta, Jr2 = q1.boxminusl(q2, Jr2=np.eye(3))
-    #         _, Jl2 = q1.boxminusl(q2, Jl2=np.eye(3))
+            theta, Jr2 = q1.boxminusl(q2, Jr2=np.eye(3))
+            _, Jl2 = q1.boxminusl(q2, Jl2=np.eye(3))
 
-    #         Jl_true = np.eye(3) @ Jr2 @ q2.Adj.T
-    #         np.testing.assert_allclose(Jl_true, Jl2)
+            Jl_true = np.eye(3) @ Jr2 @ q2.Adj.T
+            np.testing.assert_allclose(Jl_true, Jl2)
 
 
 if __name__=="__main__":