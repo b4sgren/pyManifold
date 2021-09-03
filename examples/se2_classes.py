Fixed propagation for state. Next to do covariance propagation

examples/se2_classes.py
@@ -6,7 +6,7 @@ from se2 import SE2
 class Robot:
   def __init__(self):
     self._state = SE2.Identity()
-    self._odom_cov = np.diag([.1, 1e-3])
+    self._odom_cov = np.diag([.1, .1, 1e-2])
 
   @property
   def state(self):
@@ -16,21 +16,13 @@ class Robot:
     v = 1 + .5 * np.cos(2*np.pi*0.2*t)
     w = -0.2 + 2*np.cos(2*np.pi*0.6*t)
 
-    u = np.array([v, w])
-    u_hat = u + np.random.multivariate_normal(np.zeros(2), self._odom_cov)
+    u = np.array([v, 0, w])
+    u_hat = u + np.random.multivariate_normal(np.zeros(3), self._odom_cov)
 
     return u, u_hat
 
   def propogateDynamics(self, u, dt):
-    v,w = u[0], u[1]
-    theta = SE2.Log(self._state)[2]
-
-    dx = v * np.cos(theta) * dt
-    dy = v * np.sin(theta) * dt
-    dphi = w * dt
-    t = self._state.inv().R @ np.array([dx, dy])
-    dstate = SE2.fromAngleAndt(dphi, t)
-    self._state = self._state * dstate
+    self._state = self._state.boxplusr(u*dt)
 
 class SE2_EKF:
   def __init__(self, lms):
@@ -40,8 +32,12 @@ class SE2_EKF:
   def update(self, robot, u, z):
     pass
 
-  def propogateDynamics(self, u):
-    pass # Not sure I'll need this.
+  def propogateDynamics(self, state, u, dt):
+    v, w = u[0], u[1]
+
+    t = np.array([v*dt, 0])
+    dphi = w*dt
+    dT = SE2.fromAngleAndt(dphi, t)
 
   def measurementUpdate(self, z):
     pass