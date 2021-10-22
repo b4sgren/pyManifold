e573d92b515022fa6032a3b51cb9b3e482811588
Brendon Forsgren <realpepino@gmail.com>
2021-10-22 14:20:00 -0600

    Added the history of truth and dead reckoning. Now propagate uncertainty

--- examples/error_state_ekf_quat.py
+++ examples/error_state_ekf_quat.py
@@ -35,6 +35,11 @@ class Quadrotor:
 
         # Propagate Uncertainty
 
+class EKF:
+    def __init__(self):
+        self.R_accel = np.diag([1e-3, 1e-3, 1e-3])
+        self.R_gyro = np.diag([1e-3, 1e-3, 1e-3])
+
 if __name__=="__main__":
     t0 = 0.0
     tf = 60.0
@@ -43,41 +48,89 @@ if __name__=="__main__":
     params = QuadPrams(1.0)
     traj = Trajectory(params, True)
     quad = Quadrotor(params, traj)
+    truth_quad = Quadrotor(params, traj)
+    dr_quad = Quadrotor(params, traj)
+    ekf = EKF()
 
     t_hist = np.arange(t0, tf, dt)
 
     x_hist, v_hist, euler_hist = [], [], []
+    truth_x_hist, truth_v_hist, truth_euler_hist = [], [], []
+    dr_x_hist, dr_v_hist, dr_euler_hist = [], [], []
     for t in t_hist:
         pos, v, ab, R_i_from_b, wb = traj.calcStep(t)
-        quad.propogateDynamics(ab, wb, dt)
+        eta_a = np.random.multivariate_normal(np.zeros(3), ekf.R_accel)
+        eta_g = np.random.multivariate_normal(np.zeros(3), ekf.R_gyro)
+        truth_quad.propogateDynamics(ab, wb, dt)
+        quad.propogateDynamics(ab+eta_a, wb+eta_g, dt)
+        dr_quad.propogateDynamics(ab+eta_a, wb+eta_g, dt)
 
         x_hist.append(quad.position.copy())
         v_hist.append(quad.velocity.copy())
         euler_hist.append(quad.q_i_from_b.euler)
+        truth_x_hist.append(truth_quad.position.copy())
+        truth_v_hist.append(truth_quad.velocity.copy())
+        truth_euler_hist.append(truth_quad.q_i_from_b.euler)
+        dr_x_hist.append(dr_quad.position.copy())
+        dr_v_hist.append(dr_quad.velocity.copy())
+        dr_euler_hist.append(dr_quad.q_i_from_b.euler)
+
+
 
     x_hist = np.array(x_hist).T
     v_hist = np.array(v_hist).T
     euler_hist = np.array(euler_hist).T
+    truth_x_hist = np.array(truth_x_hist).T
+    truth_v_hist = np.array(truth_v_hist).T
+    truth_euler_hist = np.array(truth_euler_hist).T
+    dr_x_hist = np.array(dr_x_hist).T
+    dr_v_hist = np.array(dr_v_hist).T
+    dr_euler_hist = np.array(dr_euler_hist).T
+
 
     fig1, ax1 = plt.subplots(nrows=3, ncols=1)
-    ax1[0].plot(t_hist, x_hist[0])
+    ax1[0].plot(t_hist, truth_x_hist[0], label='Truth')
+    ax1[1].plot(t_hist, truth_x_hist[1])
+    ax1[2].plot(t_hist, truth_x_hist[2])
+    ax1[0].plot(t_hist, x_hist[0], label='Est')
     ax1[1].plot(t_hist, x_hist[1])
     ax1[2].plot(t_hist, x_hist[2])
+    ax1[0].plot(t_hist, dr_x_hist[0], label='DR')
+    ax1[1].plot(t_hist, dr_x_hist[1])
+    ax1[2].plot(t_hist, dr_x_hist[2])
     ax1[0].set_title("Positions vs Time")
+    ax1[0].legend()
 
     fig2, ax2 = plt.subplots(nrows=3, ncols=1)
-    ax2[0].plot(t_hist, v_hist[0])
+    ax2[0].plot(t_hist, truth_v_hist[0], label='Truth')
+    ax2[1].plot(t_hist, truth_v_hist[1])
+    ax2[2].plot(t_hist, truth_v_hist[2])
+    ax2[0].plot(t_hist, v_hist[0], label='Est')
     ax2[1].plot(t_hist, v_hist[1])
     ax2[2].plot(t_hist, v_hist[2])
+    ax2[0].plot(t_hist, dr_v_hist[0], label='DR')
+    ax2[1].plot(t_hist, dr_v_hist[1])
+    ax2[2].plot(t_hist, dr_v_hist[2])
     ax2[0].set_title("Velocity vs Time")
+    ax2[0].legend()
 
     fig3, ax3 = plt.subplots(nrows=3, ncols=1)
-    ax3[0].plot(t_hist, euler_hist[0])
+    ax3[0].plot(t_hist, truth_euler_hist[0], label='Truth')
+    ax3[1].plot(t_hist, truth_euler_hist[1])
+    ax3[2].plot(t_hist, truth_euler_hist[2])
+    ax3[0].plot(t_hist, euler_hist[0], label='Est')
     ax3[1].plot(t_hist, euler_hist[1])
     ax3[2].plot(t_hist, euler_hist[2])
+    ax3[0].plot(t_hist, dr_euler_hist[0], label='DR')
+    ax3[1].plot(t_hist, dr_euler_hist[1])
+    ax3[2].plot(t_hist, dr_euler_hist[2])
     ax3[0].set_title("Euler Angles vs Time")
+    ax3[0].legend()
 
     fig4, ax4 = plt.subplots(nrows=1, ncols=1)
-    ax4.plot(x_hist[0], x_hist[1])
+    ax4.plot(truth_x_hist[0], truth_x_hist[1], label='Truth')
+    ax4.plot(x_hist[0], x_hist[1], label='Est')
+    ax4.plot(dr_x_hist[0], dr_x_hist[1], label='DR')
+    ax4.legend()
 
     plt.show()
