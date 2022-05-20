69f95e28e1071cbb457d325a997c68964836f694
Brendon Forsgren <realpepino@gmail.com>
2022-05-20 10:57:59 -0600

    Setup so that I can install the library

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pyManifold'
-    version='0.1.0'
+    name='pyManifold',
+    version='0.1.0',
     packages=find_packages(include=['pyManifold', 'pyManifold.*'])
 )
