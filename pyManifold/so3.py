fa1b3ac5451f6fc5cf501768d95e81ffa7b744aa
Brendon Forsgren <realpepino@gmail.com>
2022-10-17 10:06:33 -0500

    Changed orders of rpy angles are applied. It was (R(psi)R(theta)R(phi) and now it is R(phi)R(theta)R(psi))

--- pyManifold/so3.py
+++ pyManifold/so3.py
@@ -1,17 +1,19 @@
 import numpy as np
 
-G = np.array([[[0, 0, 0],
-                [0, 0, -1],
-                [0, 1, 0]],
-               [[0, 0, 1],
-                [0, 0, 0],
-                [-1, 0, 0]],
-               [[0, -1, 0],
-                [1, 0, 0],
-                [0, 0, 0]]])
+G = np.array(
+    [
+        [[0, 0, 0], [0, 0, -1], [0, 1, 0]],
+        [[0, 0, 1], [0, 0, 0], [-1, 0, 0]],
+        [[0, -1, 0], [1, 0, 0], [0, 0, 0]],
+    ]
+)
+
 
 def skew(qv: np.ndarray) -> np.ndarray:
-    return np.array([[0, -qv[2], qv[1]], [qv[2], 0, -qv[0]], [-qv[1], qv[0], 0]])
+    return np.array(
+        [[0, -qv[2], qv[1]], [qv[2], 0, -qv[0]], [-qv[1], qv[0], 0]]
+    )
+
 
 class SO3:
     def __init__(self, R: np.ndarray) -> None:
@@ -21,10 +23,10 @@ class SO3:
         Args:
         R -- Numpy array expressing a rotation matrix
         """
-        assert (R.shape == (3,3))
+        assert R.shape == (3, 3)
         self.arr = R
 
-    def __mul__(self, R2: 'SO3') -> 'SO3':
+    def __mul__(self, R2: "SO3") -> "SO3":
         """
         Overloaded * operator
 
@@ -48,7 +50,7 @@ class SO3:
     def __repr__(self):
         return str(self.R)
 
-    def inv(self, Jr: np.ndarray = None, Jl: np.ndarray = None) -> 'SO3':
+    def inv(self, Jr: np.ndarray = None, Jl: np.ndarray = None) -> "SO3":
         """
         Computes the inverse of the SO3 instance
 
@@ -68,11 +70,13 @@ class SO3:
         else:
             return SO3(self.arr.T)
 
-    def transpose(self) -> 'SO3':
+    def transpose(self) -> "SO3":
         """Returns the transpose of the current instance"""
         return SO3(self.arr.T)
 
-    def rota(self, v: np.ndarray, Jr: np.ndarray = None, Jl: np.ndarray = None) -> np.ndarray:
+    def rota(
+        self, v: np.ndarray, Jr: np.ndarray = None, Jl: np.ndarray = None
+    ) -> np.ndarray:
         """
         Computes the rotated vector by doing R * v
 
@@ -97,7 +101,9 @@ class SO3:
         else:
             return vp
 
-    def rotp(self, v: np.ndarray, Jr: np.ndarray = None, Jl: np.ndarray = None) -> np.ndarray:
+    def rotp(
+        self, v: np.ndarray, Jr: np.ndarray = None, Jl: np.ndarray = None
+    ) -> np.ndarray:
         """
         Computes the rotated vector by doing R^-1 * v
 
@@ -124,8 +130,9 @@ class SO3:
             return self.inv().rota(v)
 
     # Assumes jacobian is with respect to v
-    def boxplusr(self, v: np.ndarray, Jr: np.ndarray = None,
-                 Jl: np.ndarray = None) -> 'SO3':
+    def boxplusr(
+        self, v: np.ndarray, Jr: np.ndarray = None, Jl: np.ndarray = None
+    ) -> "SO3":
         """
         Computes self * Exp(v)
 
@@ -139,7 +146,7 @@ class SO3:
         Returns:
         An instance of SO3
         """
-        assert(v.size == 3)
+        assert v.size == 3
         if not Jr is None:
             R, J = SO3.Exp(v, Jr=Jr)
             res, J = self.compose(R, Jr2=J)
@@ -151,9 +158,14 @@ class SO3:
         else:
             return self * SO3.Exp(v)
 
-    def boxminusr(self, R2: 'SO3', Jr1: np.ndarray = None,
-                  Jl1: np.ndarray = None, Jr2: np.ndarray = None,
-                  Jl2: np.ndarray = None) -> np.ndarray:
+    def boxminusr(
+        self,
+        R2: "SO3",
+        Jr1: np.ndarray = None,
+        Jl1: np.ndarray = None,
+        Jr2: np.ndarray = None,
+        Jl2: np.ndarray = None,
+    ) -> np.ndarray:
         """
         Returns Log(R2.inv() * self)
 
@@ -187,8 +199,9 @@ class SO3:
         else:
             return SO3.Log(R2.inv() * self)
 
-    def boxplusl(self, v: np.ndarray, Jr: np.ndarray = None,
-                 Jl: np.ndarray = None) -> 'SO3':
+    def boxplusl(
+        self, v: np.ndarray, Jr: np.ndarray = None, Jl: np.ndarray = None
+    ) -> "SO3":
         """
         Returns Exp(v) * self
 
@@ -202,7 +215,7 @@ class SO3:
         Returns:
         An instance of SO3
         """
-        assert(v.size == 3)
+        assert v.size == 3
         if Jr is not None:
             R, J = SO3.Exp(v, Jr=Jr)
             return R.compose(self, Jr=J)
@@ -212,9 +225,14 @@ class SO3:
         else:
             return SO3.Exp(v) * self
 
-    def boxminusl(self, R2: 'SO3', Jr1: np.ndarray = None,
-                  Jl1: np.ndarray = None, Jr2: np.ndarray = None,
-                  Jl2: np.ndarray = None):
+    def boxminusl(
+        self,
+        R2: "SO3",
+        Jr1: np.ndarray = None,
+        Jl1: np.ndarray = None,
+        Jr2: np.ndarray = None,
+        Jl2: np.ndarray = None,
+    ):
         """
         Returns Log(R2.inv() * self)
 
@@ -251,9 +269,9 @@ class SO3:
 
     def normalize(self) -> None:
         """Normalizes the rotation matrix"""
-        x = self.R[:,0]
+        x = self.R[:, 0]
         x = x / np.linalg.norm(x)
-        y = np.cross(self.R[:,2], x)
+        y = np.cross(self.R[:, 2], x)
         y = y / np.linalg.norm(y)
         z = np.cross(x, y)
 
@@ -263,9 +281,14 @@ class SO3:
         """Computes the determinant"""
         return np.linalg.det(self.R)
 
-    def compose(self, R: 'SO3', Jr: np.ndarray = None,
-                Jl: np.ndarray = None, Jr2: np.ndarray = None,
-                Jl2: np.ndarray = None):
+    def compose(
+        self,
+        R: "SO3",
+        Jr: np.ndarray = None,
+        Jl: np.ndarray = None,
+        Jr2: np.ndarray = None,
+        Jl2: np.ndarray = None,
+    ):
         """
         Alternate way to * operator to compose two matrices. This allows for calculating the jacobians
 
@@ -302,22 +325,28 @@ class SO3:
     @property
     def euler(self) -> np.ndarray:
         """Returns the RPY angles for the rotation matrix"""
-        if 1 - np.abs(self.R[2,0]) > 1e-8:
-            phi = np.arctan2(self.R[2,1], self.R[2,2])
-            theta = np.arcsin(-self.R[2,0])
-            psi = np.arctan2(self.R[1,0], self.R[0,0])
-        else:
-            phi = 0
-            if np.sign(self.R[2,0]) > 0:
-                theta = np.pi/2
-                psi = -np.arctan2(-self.R[1,2], self.R[1,1])
-            else:
-                theta = -np.pi/2
-                psi = np.arctan2(-self.R[1,2], self.R[1,1])
+        # if 1 - np.abs(self.R[2, 0]) > 1e-8:
+        #     phi = np.arctan2(self.R[2, 1], self.R[2, 2])
+        #     theta = np.arcsin(-self.R[2, 0])
+        #     psi = np.arctan2(self.R[1, 0], self.R[0, 0])
+        # else:
+        #     phi = 0
+        #     if np.sign(self.R[2, 0]) > 0:
+        #         theta = np.pi / 2
+        #         psi = -np.arctan2(-self.R[1, 2], self.R[1, 1])
+        #     else:
+        #         theta = -np.pi / 2
+        #         psi = np.arctan2(-self.R[1, 2], self.R[1, 1])
+        # return np.array([phi, theta, psi])
+        phi = np.arctan2(self.R[2, 0], self.R[2, 1])
+        theta = np.arccos(self.R[2, 2])
+        psi = np.arctan2(selfR[0, 2], self.R[1, 2])
+
         return np.array([phi, theta, psi])
 
+    # Not sure that this does what I want. It may need to be reversed R3*R2*R1
     @classmethod
-    def fromRPY(cls, angles: np.ndarray) -> 'SO3':
+    def fromRPY(cls, angles: np.ndarray) -> "SO3":
         """
         Creates an SO3 instance from roll, pitch, yaw angles
 
@@ -343,10 +372,11 @@ class SO3:
         sp = np.sin(phi)
         R3 = np.array([[1, 0, 0], [0, cp, -sp], [0, sp, cp]])
 
-        return cls(R1 @ R2 @ R3)
+        # return cls(R1 @ R2 @ R3)
+        return cls(R3 @ R2 @ R1)
 
     @classmethod
-    def fromAxisAngle(cls, w: np.ndarray) -> 'SO3':
+    def fromAxisAngle(cls, w: np.ndarray) -> "SO3":
         """
         Creates an instance of SO3 from an Axis Angle Formulation
 
@@ -357,21 +387,23 @@ class SO3:
         An instance of SO3
         """
         theta = np.linalg.norm(w)
-        skew_w = np.array([[0, -w[2], w[1]], [w[2], 0, -w[0]], [-w[1], w[0], 0]])
+        skew_w = np.array(
+            [[0, -w[2], w[1]], [w[2], 0, -w[0]], [-w[1], w[0], 0]]
+        )
 
         if np.abs(theta) > 1e-8:
             A = np.sin(theta) / theta
             B = (1 - np.cos(theta)) / (theta**2)
         else:
             A = 1.0 - theta**2 / 6.0 + theta**4 / 120.0
-            B = 0.5 - theta**2 / 24.0 + theta**4/720.0
+            B = 0.5 - theta**2 / 24.0 + theta**4 / 720.0
 
         arr = np.eye(3) + A * skew_w + B * (skew_w @ skew_w)
 
         return cls(arr)
 
     @classmethod
-    def fromQuaternion(cls, q: np.ndarray) -> 'SO3':
+    def fromQuaternion(cls, q: np.ndarray) -> "SO3":
         """
         Creates an instance of SO3 from a quaternion
 
@@ -383,32 +415,50 @@ class SO3:
         """
         qw = q[0]
         qv = q[1:]
-        qv_skew = np.array([[0, -qv[2], qv[1]], [qv[2], 0, -qv[0]], [-qv[1], qv[0], 0]])
+        qv_skew = np.array(
+            [[0, -qv[2], qv[1]], [qv[2], 0, -qv[0]], [-qv[1], qv[0], 0]]
+        )
 
-        R = (2 * qw**2 - 1) * np.eye(3) + 2 * qw * qv_skew + 2 * np.outer(qv, qv)
+        R = (
+            (2 * qw**2 - 1) * np.eye(3)
+            + 2 * qw * qv_skew
+            + 2 * np.outer(qv, qv)
+        )
         return cls(R)
 
     @classmethod
-    def random(cls) -> 'SO3':
+    def random(cls) -> "SO3":
         """
         Returns a randomly generated rotation matrix
         """
         x = np.random.uniform(0, 1, size=3)
         psi = 2 * np.pi * x[0]
-        R = np.array([[np.cos(psi), np.sin(psi), 0], [-np.sin(psi), np.cos(psi), 0], [0, 0, 1]])
-        v = np.array([np.cos(2 * np.pi * x[1]) * np.sqrt(x[2]),
-                     np.sin(2 * np.pi * x[1]) * np.sqrt(x[2]),
-                     np.sqrt(1 - x[2])])
+        R = np.array(
+            [
+                [np.cos(psi), np.sin(psi), 0],
+                [-np.sin(psi), np.cos(psi), 0],
+                [0, 0, 1],
+            ]
+        )
+        v = np.array(
+            [
+                np.cos(2 * np.pi * x[1]) * np.sqrt(x[2]),
+                np.sin(2 * np.pi * x[1]) * np.sqrt(x[2]),
+                np.sqrt(1 - x[2]),
+            ]
+        )
         H = np.eye(3) - 2 * np.outer(v, v)
         return cls(-H @ R)
 
     @staticmethod
-    def Identity() -> 'SO3':
+    def Identity() -> "SO3":
         """Returns the identity matrix"""
         return SO3(np.eye(3))
 
     @staticmethod
-    def log(R: 'SO3', Jr: np.ndarray = None, Jl: np.ndarray = None) -> np.ndarray: #This function isn't entirely stable but tests pass
+    def log(
+        R: "SO3", Jr: np.ndarray = None, Jl: np.ndarray = None
+    ) -> np.ndarray:  # This function isn't entirely stable but tests pass
         """
         Performs the logarithmic map to convert SO3 elements to its tangent space
 
@@ -424,29 +474,52 @@ class SO3:
         """
         assert isinstance(R, SO3)
 
-        theta = np.arccos((np.trace(R.arr) - 1)/2.0)
-        if np.abs(theta) < 1e-8: # Do taylor series expansion
-            temp = 1/2.0 * (1 + theta**2 / 6.0 + 7 * theta**4 / 360)
+        theta = np.arccos((np.trace(R.arr) - 1) / 2.0)
+        if np.abs(theta) < 1e-8:  # Do taylor series expansion
+            temp = 1 / 2.0 * (1 + theta**2 / 6.0 + 7 * theta**4 / 360)
             logR = temp * (R - R.transpose())
         elif np.abs(np.abs(theta) - np.pi) < 1e-3:
-            temp = - np.pi/(theta - np.pi) - 1 - np.pi/6 * (theta - np.pi) - (theta - np.pi)**2/6 - 7*np.pi/360 * (theta - np.pi)**3 - 7/360.0 * (theta - np.pi)**4
-            logR = temp/2.0 * (R - R.transpose())
+            temp = (
+                -np.pi / (theta - np.pi)
+                - 1
+                - np.pi / 6 * (theta - np.pi)
+                - (theta - np.pi) ** 2 / 6
+                - 7 * np.pi / 360 * (theta - np.pi) ** 3
+                - 7 / 360.0 * (theta - np.pi) ** 4
+            )
+            logR = temp / 2.0 * (R - R.transpose())
         else:
             logR = theta / (2.0 * np.sin(theta)) * (R - R.transpose())
 
-        if not Jr is None: # TODO: Add Taylor series expansion?
+        if not Jr is None:  # TODO: Add Taylor series expansion?
             thetax = skew(SO3.vee(logR))
-            J = np.eye(3) + 0.5 * thetax + (1/theta**2 - (1 + np.cos(theta))/(2 * theta * np.sin(theta))) * (thetax @ thetax)
+            J = (
+                np.eye(3)
+                + 0.5 * thetax
+                + (
+                    1 / theta**2
+                    - (1 + np.cos(theta)) / (2 * theta * np.sin(theta))
+                )
+                * (thetax @ thetax)
+            )
             return logR, J @ Jr
         elif not Jl is None:
             thetax = skew(SO3.vee(logR))
-            J = np.eye(3) - 0.5 * thetax + (1/theta**2 - (1 + np.cos(theta))/(2 * theta * np.sin(theta))) * (thetax @ thetax)
+            J = (
+                np.eye(3)
+                - 0.5 * thetax
+                + (
+                    1 / theta**2
+                    - (1 + np.cos(theta)) / (2 * theta * np.sin(theta))
+                )
+                * (thetax @ thetax)
+            )
             return logR, J @ Jl
         else:
             return logR
 
-    @classmethod # Make this method the above
-    def Log(cls, R, Jr=None, Jl=None): #easy call to go straight to a vector
+    @classmethod  # Make this method the above
+    def Log(cls, R, Jr=None, Jl=None):  # easy call to go straight to a vector
         if not Jr is None:
             logR, J = cls.log(R, Jr=Jr)
             return cls.vee(logR), J
@@ -458,8 +531,9 @@ class SO3:
             return cls.vee(logR)
 
     @classmethod
-    def exp(cls, logR: np.ndarray, Jr: np.ndarray = None,
-            Jl: np.ndarray = None) -> 'SO3':
+    def exp(
+        cls, logR: np.ndarray, Jr: np.ndarray = None, Jl: np.ndarray = None
+    ) -> "SO3":
         """
         Computes the exponential map to convert elements of the tangent space to the manifold
 
@@ -473,16 +547,20 @@ class SO3:
         Returns:
         An instance of SO3
         """
-        assert logR.shape == (3,3)
+        assert logR.shape == (3, 3)
 
         w = cls.vee(logR)
         theta = np.sqrt(w @ w)
         if np.abs(theta) > 1e-8:
-            R = np.eye(3) + np.sin(theta)/theta * logR + (1 - np.cos(theta))/ (theta**2) * (logR @ logR)
-        else: # Do taylor series expansion for small thetas
+            R = (
+                np.eye(3)
+                + np.sin(theta) / theta * logR
+                + (1 - np.cos(theta)) / (theta**2) * (logR @ logR)
+            )
+        else:  # Do taylor series expansion for small thetas
             R = np.eye(3)
 
-        if not Jr is None: # Possibly add taylor series logic
+        if not Jr is None:  # Possibly add taylor series logic
             wx = skew(w)
             a = (1 - np.cos(theta)) / theta**2
             b = (theta - np.sin(theta)) / theta**3
@@ -497,7 +575,7 @@ class SO3:
         else:
             return cls(R)
 
-    @classmethod # Do this in the above method
+    @classmethod  # Do this in the above method
     def Exp(cls, w, Jr=None, Jl=None):
         logR = cls.hat(w)
         if not Jr is None:
@@ -520,8 +598,8 @@ class SO3:
         Returns:
         The vector used to make logR
         """
-        assert logR.shape == (3,3)
-        omega = np.array([logR[2,1], logR[0,2], logR[1,0]])
+        assert logR.shape == (3, 3)
+        omega = np.array([logR[2, 1], logR[0, 2], logR[1, 0]])
         return omega
 
     @staticmethod
--- tests/test_so3.py
+++ tests/test_so3.py
@@ -3,11 +3,13 @@ import scipy as sp
 from scipy.spatial.transform import Rotation
 import unittest
 import sys
+
 sys.path.append("..")
-from so3 import SO3
-from quaternion import Quaternion
+from pyManifold.so3 import SO3
+from pyManifold.quaternion import Quaternion
 
 from IPython.core.debugger import Pdb
+
 
 class SO3_testing(unittest.TestCase):
     def testConstructor(self):
@@ -27,14 +29,16 @@ class SO3_testing(unittest.TestCase):
             sps = np.sin(angles[2])
             R3 = np.array([[cps, -sps, 0], [sps, cps, 0], [0, 0, 1]])
 
-            R_true = Rotation.from_euler('ZYX', [angles[2], angles[1], angles[0]]).as_matrix()
+            R_true = Rotation.from_euler(
+                "ZYX", [angles[2], angles[1], angles[0]]
+            ).as_matrix()
 
             R_ex2 = SO3(R_true)
 
             np.testing.assert_allclose(R_true, R_ex.arr)
             np.testing.assert_allclose(R_true, R_ex2.arr)
 
-    def testLog(self): #This has issues sometimes. It is infrequent though
+    def testLog(self):  # This has issues sometimes. It is infrequent though
         for i in range(100):
             temp = Rotation.random().as_matrix()
             R = SO3(temp)
@@ -42,7 +46,7 @@ class SO3_testing(unittest.TestCase):
             logR = SO3.log(R)
             logR_true = sp.linalg.logm(temp)
 
-            if np.linalg.norm(logR_true - logR, ord='fro') > 1e-3:
+            if np.linalg.norm(logR_true - logR, ord="fro") > 1e-3:
                 Pdb().set_trace()
                 debug = 1
                 temp = SO3.log(R)
@@ -50,7 +54,7 @@ class SO3_testing(unittest.TestCase):
             np.testing.assert_allclose(logR_true, logR, atol=1e-10)
 
     def testTaylorLog(self):
-        for i in range(100):  #Around 0
+        for i in range(100):  # Around 0
             vec = np.random.uniform(-3.0, 3.0, size=3)
             vec = vec / np.linalg.norm(vec)
             ang = np.random.uniform(-1e-8, 1e-3)
@@ -60,15 +64,14 @@ class SO3_testing(unittest.TestCase):
             logR = SO3.log(R)
             logR_true = sp.linalg.logm(R.R)
 
-            if np.linalg.norm(logR_true - logR, ord='fro') > 1e-8:
+            if np.linalg.norm(logR_true - logR, ord="fro") > 1e-8:
                 Pdb().set_trace()
                 debug = 1
                 temp = SO3.log(R)
 
-
             np.testing.assert_allclose(logR_true, logR, atol=1e-10)
 
-        for i in range(100): #Around pi
+        for i in range(100):  # Around pi
             vec = np.random.uniform(-1.0, 1.0, size=3)
             ang = np.random.uniform(-0, 1e-8)
             vec = vec / np.linalg.norm(vec) * (np.pi - ang)
@@ -79,43 +82,54 @@ class SO3_testing(unittest.TestCase):
             logR_true = sp.linalg.logm(R.R)
 
             # if np.linalg.norm(logR_true - logR, ord='fro') > 1e-8:
-                # Pdb().set_trace()
-                # debug = 1
-                # temp = SO3.log(R)
+            # Pdb().set_trace()
+            # debug = 1
+            # temp = SO3.log(R)
 
             # np.testing.assert_allclose(logR_true, logR, atol=1e-10)
-            #This test has issues. Same with this part in SE3
+            # This test has issues. Same with this part in SE3
 
     def testExp(self):
         for i in range(100):
             logR_vec = np.random.uniform(-np.pi, np.pi, size=3)
-            logR = np.array([[0, -logR_vec[2], logR_vec[1]],
-                        [logR_vec[2], 0, -logR_vec[0]],
-                        [-logR_vec[1], logR_vec[0], 0]])
+            logR = np.array(
+                [
+                    [0, -logR_vec[2], logR_vec[1]],
+                    [logR_vec[2], 0, -logR_vec[0]],
+                    [-logR_vec[1], logR_vec[0], 0],
+                ]
+            )
 
             R = SO3.exp(logR)
             R_true = sp.linalg.expm(logR)
 
             np.testing.assert_allclose(R_true, R.arr)
 
-        for i in range(100): #Test taylor series
+        for i in range(100):  # Test taylor series
             logR_vec = np.random.uniform(0.0, 1e-8, size=3)
-            logR = np.array([[0, -logR_vec[2], logR_vec[1]],
-                        [logR_vec[2], 0, -logR_vec[0]],
-                        [-logR_vec[1], logR_vec[0], 0]])
+            logR = np.array(
+                [
+                    [0, -logR_vec[2], logR_vec[1]],
+                    [logR_vec[2], 0, -logR_vec[0]],
+                    [-logR_vec[1], logR_vec[0], 0],
+                ]
+            )
 
             R = SO3.exp(logR)
             R_true = sp.linalg.expm(logR)
 
             np.testing.assert_allclose(R_true, R.arr, atol=1e-7)
 
-
     def testVee(self):
         for i in range(100):
             omega_true = np.random.uniform(-np.pi, np.pi, size=3)
-            logR = np.array([[0, -omega_true[2], omega_true[1]],
-                            [omega_true[2], 0, -omega_true[0]],
-                            [-omega_true[1], omega_true[0], 0]])
+            logR = np.array(
+                [
+                    [0, -omega_true[2], omega_true[1]],
+                    [omega_true[2], 0, -omega_true[0]],
+                    [-omega_true[1], omega_true[0], 0],
+                ]
+            )
 
             omega = SO3.vee(logR)
             np.testing.assert_allclose(omega_true, omega)
@@ -124,9 +138,13 @@ class SO3_testing(unittest.TestCase):
         for i in range(100):
             omega = np.random.uniform(-np.pi, np.pi, size=3)
 
-            logR_true = np.array([[0, -omega[2], omega[1]],
-                                [omega[2], 0, -omega[0]],
-                                [-omega[1], omega[0], 0]])
+            logR_true = np.array(
+                [
+                    [0, -omega[2], omega[1]],
+                    [omega[2], 0, -omega[0]],
+                    [-omega[1], omega[0], 0],
+                ]
+            )
 
             logR = SO3.hat(omega)
 
@@ -144,14 +162,14 @@ class SO3_testing(unittest.TestCase):
             np.testing.assert_allclose(R_inv_true, R_inv.R)
             np.testing.assert_allclose(R_inv_true, R_T.R)
 
-    def testFromAxisEuler(self):
+    def testFromAxisAngle(self):
         for i in range(100):
             theta = np.random.uniform(-np.pi, np.pi)
             vec = np.random.uniform(-1, 1, size=3)
             vec = vec / np.linalg.norm(vec)
 
             R = SO3.fromAxisAngle(theta * vec)
-            R_true = Rotation.from_rotvec(vec*theta).as_matrix()
+            R_true = Rotation.from_rotvec(vec * theta).as_matrix()
 
             np.testing.assert_allclose(R_true, R.arr)
 
@@ -169,7 +187,7 @@ class SO3_testing(unittest.TestCase):
             np.testing.assert_allclose(R3_true, R3.arr)
 
     def testRotatingVector(self):
-        for i in range(100): #Active rotation
+        for i in range(100):  # Active rotation
             rot1 = Rotation.random().as_matrix()
 
             R = SO3(rot1)
@@ -268,7 +286,7 @@ class SO3_testing(unittest.TestCase):
         for i in range(100):
             theta = np.random.uniform(-np.pi, np.pi)
             vec = np.random.uniform(-10.0, 10.0, size=3)
-            vec = vec/np.linalg.norm(vec)
+            vec = vec / np.linalg.norm(vec)
 
             q = Quaternion.fromAxisAngle(vec * theta)
 
@@ -349,9 +367,9 @@ class SO3_testing(unittest.TestCase):
             v = np.random.uniform(-10, 10, size=3)
 
             vp, Jr = R.rota(v, Jr=np.eye(3))
-            vx = np.array([[0, -v[2], v[1]],
-                           [v[2], 0, -v[0]],
-                           [-v[1], v[0], 0]])
+            vx = np.array(
+                [[0, -v[2], v[1]], [v[2], 0, -v[0]], [-v[1], v[0], 0]]
+            )
             Jr_true = -R.R @ vx
 
             np.testing.assert_allclose(Jr_true, Jr)
@@ -387,9 +405,9 @@ class SO3_testing(unittest.TestCase):
             v = np.random.uniform(-10, 10, size=3)
 
             vp, Jr = R.rotp(v, Jr=np.eye(3))
-            Jr_true = np.array([[0, -vp[2], vp[1]],
-                                [vp[2], 0, -vp[0]],
-                                [-vp[1], vp[0], 0]])
+            Jr_true = np.array(
+                [[0, -vp[2], vp[1]], [vp[2], 0, -vp[0]], [-vp[1], vp[0], 0]]
+            )
 
             np.testing.assert_allclose(Jr_true, Jr, atol=1e-10)
 
@@ -399,9 +417,9 @@ class SO3_testing(unittest.TestCase):
             v = np.random.uniform(-10, 10, size=3)
 
             vp, Jl = R.rotp(v, Jl=np.eye(3))
-            vx = np.array([[0, -v[2], v[1]],
-                            [v[2], 0, -v[0]],
-                            [-v[1], v[0], 0]])
+            vx = np.array(
+                [[0, -v[2], v[1]], [v[2], 0, -v[0]], [-v[1], v[0], 0]]
+            )
             Jl_true = R.R.T @ vx
 
             np.testing.assert_allclose(Jl_true, Jl)
@@ -486,5 +504,5 @@ class SO3_testing(unittest.TestCase):
             np.testing.assert_allclose(R1.R, R2.R, rtol=1e-5)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     unittest.main()
